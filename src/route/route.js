43d616ca5fa006401120ca1db0c3b7f6907908ee
rupeshhardas <rupesh7875804349@gmail.com>
2022-04-08 17:28:28 +0530

    project done

--- src/route/route.js
+++ src/route/route.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 
-const urlController = require("../controolers/urlController")
+const urlController = require("../controllers/urlController")
 
 
 
